fbb4932b3ddf0d76002895413951b43cd7ded914
Santiago Botero Correa <sabotero@gmail.com>
2019-01-10 09:53:42 +0100

    day 5

--- Day05.fsx
+++ Day05.fsx
@@ -6,20 +6,21 @@ let react (unitA : char) (unitB : char) =
     unitA <> unitB && 
     Char.ToLower(unitA) = Char.ToLower(unitB)
 
-let calculateRactions (rawPolymer : string) =
+let calculateReactions (rawPolymer : string) =
     let rec loop (polymer : string) i (previousUnit : char) (acc : char list) count =
         match previousUnit with
-        | c when i = polymer.Length && count > 0 ->
+        | c when i >= polymer.Length && count > 0 ->
             let newPolymer = List.rev (c::acc) |> fun chars -> String.Join("",chars)
             loop newPolymer 1 newPolymer.[0] [] 0
-        | c when i = polymer.Length -> c::acc |> List.rev
-        | c when react c polymer.[i] -> 
-            loop polymer (i + 2) polymer.[i + 1] acc (count + 1)
+        | c when i >= polymer.Length -> c::acc |> List.rev
+        | c when react c polymer.[i] ->
+            let j = if i + 1 = polymer.Length then i else i + 1
+            loop polymer (i + 2) polymer.[j] acc (count + 1)
         | c -> loop polymer (i + 1) polymer.[i] (c::acc) count
     loop rawPolymer 1 rawPolymer.[0] [] 0
 
 "dabAcCaCBAcCcaDA"
-|> calculateRactions
+|> calculateReactions
 |> fun chars -> System.String.Join("", chars)
 |> fun actual ->
     let expected = "dabCBAcaDA"
@@ -29,9 +30,39 @@ let calculateRactions (rawPolymer : string) =
 
 let treatData data =
     data
-    |> calculateRactions
+    |> calculateReactions
     |> fun chars -> System.String.Join("", chars)
     |> fun s -> s.Length
 
 treatData "dabAcCaCBAcCcaDA"
 treatData input.[0]
+
+open System.Text.RegularExpressions
+
+let replaceIgnoreCase (newValue : string) (oldValuie:string) (s:string) =
+    Regex.Replace(s, oldValuie, newValue, RegexOptions.IgnoreCase)
+
+let removeIgnoreCase (oldValuie:string) (s:string) = replaceIgnoreCase "" oldValuie s
+
+let reactUnits unit rawPolymer = replaceIgnoreCase unit rawPolymer
+
+let fullyReactPolymer rawPolymer =
+    let keepShortetsPolymer (unitA, shortest) (unitB, candidatelength) =
+        if shortest <= candidatelength 
+        then (unitA, shortest)
+        else (unitB, candidatelength)
+
+    let rec loop i (units : string) (acc : char * int) =
+        if i = units.Length
+        then acc
+        else
+            let fullyReactedPolymerLength =
+                rawPolymer
+                |> removeIgnoreCase (units.[i].ToString())
+                |> treatData
+            
+            keepShortetsPolymer acc (units.[i], fullyReactedPolymerLength)
+            |> loop (i + 1) units
+    loop 0 "abcdefghijklmnopqrstuvwxyz" ('+', Int32.MaxValue)
+
+fullyReactPolymer input.[0]
